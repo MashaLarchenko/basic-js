d02a0373c82798b38804115b83696bd2fa62c0c0
Maryia Larchanka <mashalarchenko16@gmail.com>
2020-09-07 21:50:53 +0300

    feat: implement task6

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,36 @@
-const CustomError = require("../extensions/custom-error");
-
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function transform(arr) {
+  const controlsSequences = [
+    '--discard-next',
+    '--discard-prev',
+    '--double-next',
+    '--double-prev',
+  ];
+  if (Array.isArray(arr)) {
+    const transformedArr = [];
+    let next = false;
+    for (let i = 0; i < arr.length; i++) {
+      if (next) {
+        next = false
+      } else if (arr[i] === '--discard-prev' && arr[i - 2] !== '--discard-next') {
+        if (i !== 0) {
+          transformedArr.pop();
+        }
+      } else if (arr[i] === '--discard-next') {
+        next = true;
+      } else if (arr[i] === '--double-next') {
+        if (i + 1 !== arr.length - 1 && arr[i + 1 ] !== undefined) {
+          transformedArr.push(arr[i + 1]);
+        }
+      } else if (arr[i] === '--double-prev' && arr[i - 2] !== '--discard-next') {
+        if (i !== 0 && arr[i - 1 ] !== undefined) {
+          transformedArr.push(arr[i - 1]);
+        }
+      } else {
+        if (!controlsSequences.includes(arr[i])) {
+          transformedArr.push(arr[i]);
+        }
+      }
+    }
+    return transformedArr;
+  } else throw new Error()
+}
