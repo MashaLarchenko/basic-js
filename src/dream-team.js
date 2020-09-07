175a13a48ede926796482cc3ef9d69342090b602
Maryia Larchanka <mashalarchenko16@gmail.com>
2020-09-07 18:26:32 +0300

    feat: implement task3

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,16 @@
-const CustomError = require("../extensions/custom-error");
-
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  const unsortedArr = [];
+  if (Array.isArray(members)) {
+    members.forEach(member => {
+      if (typeof member === 'string') {
+        const arrFromName = member.trim().split(' ');
+        unsortedArr.push(arrFromName[0].toUpperCase().charCodeAt([0]));
+      }
+    });
+    const sortedArr = unsortedArr.sort((a, b) => a - b);
+    const result = sortedArr.reduce((acc, item) => {
+      return acc += String.fromCharCode(item);
+    }, '')
+    return result;
+  } else return false;
 };
