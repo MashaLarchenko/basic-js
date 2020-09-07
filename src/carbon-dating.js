d71664deb4bfe72b31dec15d38db86482d9e52b2
Maryia Larchanka <mashalarchenko16@gmail.com>
2020-09-07 17:55:38 +0300

    feat: implement task2

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,9 +1,13 @@
-const CustomError = require("../extensions/custom-error");
+const MODERN_ACTIVITY = 15;
+const HALF_LIFE_PERIOD = 5730;
 
-const MODERN_ACTIVITY= 15; 
-const HALF_LIFE_PERIOD= 5730;
-
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+  if (typeof sampleActivity === 'string') {
+    const activity = parseFloat(sampleActivity);
+    if (activity <= 0 || activity > MODERN_ACTIVITY || isNaN(activity)) return false;
+    const coef = Math.LN2.toFixed(3) / HALF_LIFE_PERIOD;
+    return Math.ceil(Math.log(MODERN_ACTIVITY / activity) / coef);
+  } else {
+    return false;
+  }
 };
